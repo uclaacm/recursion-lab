2614a09c061994097d2f968b025d0108c5962363
Bryan Pan <bryanpan342@gmail.com>
2021-06-18 15:46:41 -0700

    fix: allow for enum in typescript (#17)
    In order to use enums in typescript with the raw eslint config, you have to delete the base `no-shadow` requirement.
    
    See [here](https://stackoverflow.com/questions/63961803/eslint-says-all-enums-in-typescript-app-are-already-declared-in-the-upper-scope)
    
    Co-authored-by: Matthew Nieva <65370631+matthewcn56@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,7 +68,9 @@ module.exports = {
     'no-duplicate-imports': ['error'],
 
     // Cannot shadow names
-    '@typescript-eslint/no-shadow': ['error'],
+    // note you must disable the base rule as it can report incorrect errors
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
 
     // Required spacing in property declarations (copied from TSLint, defaults are good)
     'key-spacing': ['error'],
