ef0e94cc3328db42697564e8078431062d249a08
archishadatta <44505845+archishadatta@users.noreply.github.com>
2021-07-24 00:13:58 -0700

    Update footer font and logo  (#33)
    * footer content
    
    * font fix now

--- src/components/shared/AppWrapper/Footer.tsx
+++ src/components/shared/AppWrapper/Footer.tsx
@@ -3,15 +3,17 @@ function Footer(): JSX.Element {
   return (
     <div id="footer">
       <h3>
-        made with ❤️ by
+      made with ❤️ by
         {' '}
         <a
           href="https://teachla.uclaacm.com"
           target="_blank"
           rel="noopener noreferrer"
+          id="footer-link"
         >
           {' '}
           <img id="tla-logo" src={TeachLALogo} alt="teach la logo!" />
+          acm.teachLA
         </a>
       </h3>
     </div>
