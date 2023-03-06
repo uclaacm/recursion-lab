1a806443db2f7b4f379c7c002a13b86264c43ca9
Edwin Montalvo <edwinmontavlo123@gmail.com>
2023-03-05 23:30:54 -0800

    Update icons of sidebar

--- src/components/shared/SidebarData.tsx
+++ src/components/shared/SidebarData.tsx
@@ -1,41 +1,48 @@
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHouse,
+  faClipboard,
+  faBook,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import mario from '../../assets/mario.png';
+import rabbit from '../../assets/rabbit.png';
+import telephone from '../../assets/telephone.png';
 
 export const SidebarData = [
   {
     title: 'Home',
     path: '/',
-    icons: <FontAwesomeIcon icon={faBars} />,
+    icons: <FontAwesomeIcon icon={faHouse} size="2x" />,
     cName: 'nav-text',
   },
   {
     title: 'Introduction',
     path: '/intro',
-    icons: <FontAwesomeIcon icon={faBars} />,
+    icons: <FontAwesomeIcon icon={faClipboard} size="2x" />,
     cName: 'nav-text',
   },
   {
     title: 'Problem 1',
     path: '/problem1',
-    icons: <FontAwesomeIcon icon={faBars} />,
+    icons: <img src={mario} width="54" alt="mario-face" />,
     cName: 'nav-text',
   },
   {
     title: 'Problem 2',
     path: '/problem2',
-    icons: <FontAwesomeIcon icon={faBars} />,
+    icons: <img src={telephone} width="54" alt="telephone-lady" />,
     cName: 'nav-text',
   },
   {
     title: 'Problem 3',
     path: '/problem3',
-    icons: <FontAwesomeIcon icon={faBars} />,
+    icons: <img src={rabbit} width="54" alt="rabbit-face" />,
     cName: 'nav-text',
   },
   {
     title: 'Problem 4',
     path: '/problem4',
-    icons: <FontAwesomeIcon icon={faBars} />,
+    icons: <FontAwesomeIcon icon={faBook} size="2x" />,
     cName: 'nav-text',
   },
 ];
