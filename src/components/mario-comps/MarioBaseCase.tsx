f50418c9ed10c4aae41a6b677de9e14c9b6d4fa2
Edwin Montalvo <edwinmontavlo123@gmail.com>
2023-04-14 15:59:03 -0700

    Finish KhanCard styling and functionality

--- src/components/mario-comps/MarioBaseCase.tsx
+++ src/components/mario-comps/MarioBaseCase.tsx
@@ -2,8 +2,15 @@ import { useState } from 'react';
 import KhanCard from '../shared/KhanCard';
 import KhanInput from '../shared/KhanInput';
 
+/*
+How to use the KhanCard and corresponding KhanInput component:
+  KhanCard = wrapper box that holds the content that you want displayed
+  KhanInput = a styled input box
+  NOTE: parent component (MarioBaseCase) holds state that KhanCard and KhanInput will be using
+*/
+
 function MarioBaseCase(): JSX.Element {
-  const [isCorrect] = useState([false, false]); //parent is keeping track of what questions are correct
+  const [isCorrect] = useState([false, false]); //state for if questions are correct
   const [answerKey] = useState({
     question1: 'answer1',
     question2: 'answer2',
@@ -12,17 +19,17 @@ function MarioBaseCase(): JSX.Element {
   return (
     <div>
       <KhanCard
-        correct="mario stuff correct"
-        incorrect="mario stuff incorrect"
-        correct_answer={isCorrect}
-        index={0}
+        correct="mario stuff correct" //explanation for when user gets answer correct
+        incorrect="mario stuff incorrect" //explanation/hint when user gets answer incorrect
+        correct_answer={isCorrect} //must pass in correctness state from parent
+        index={0} //index for what KhanCard you are referring to
       >
         <div>
           <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</span>
           <KhanInput
-            correct_answer={isCorrect}
-            index={0}
-            answer={answerKey.question1}
+            correct_answer={isCorrect} //must pass in correctness state from parent
+            index={0} //index for what KhanCard you are referring to
+            answer={answerKey.question1} //the answer from answerkey
           />
         </div>
       </KhanCard>
