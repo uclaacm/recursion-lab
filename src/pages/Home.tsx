144ff8b06de7377e7ac55934b1506307ea49b15f
rosiechen1005 <chenherosalinda@gmail.com>
2023-04-29 23:18:16 -0700

    haven't updated the rabbits correctly yet, just trying to make pull request

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,33 @@
 import { faGreaterThan } from '@fortawesome/free-solid-svg-icons';
 import { FC } from 'react';
-import Button from '../components/shared/Button';
+import centerRabbit from '../assets/center_rabbit.png';
 import '../styles/Home.scss';
+import '../styles/Mario.scss';
+import '../styles/Rabbits.scss';
+import homeBackground from '../assets/home_page.png';
+import marioHomepage from '../assets/mario_homepage.png';
+import startButton from '../assets/start_button.png';
+import title from '../assets/title.png';
+import Button from '../components/shared/Button';
 
 const Home: FC = () => {
   return (
-    <div className="home-container">
-      <span>Home Page.</span>
+    <div className="home-container" style={{ backgroundImage: `url(${homeBackground})`, backgroundSize: 'cover' }} >
       <Button icon={faGreaterThan} className="next-btn" navigation="/intro" />
+      <div className="start-button-container">
+        <img className="start-button" src={startButton} alt="Start Button" />
+      </div>
+      <div>
+      <img className="mario-image" src={marioHomepage} alt="Mario" />
+      </div>
+      <div>
+      <img className="title" src={title} alt="Title" />
+      </div>
+      <div>
+      <img className="center-rabbit" src={centerRabbit} alt="Center Rabbit" />
+      </div>
     </div>
+
   );
 };
 
