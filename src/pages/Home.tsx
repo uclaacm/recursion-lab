e0735f8d6f93b9aa6f44354aaf235887d4595444
rosiechen1005 <130522644+rosiechen1005@users.noreply.github.com>
2023-05-05 14:01:58 -0700

    Finish home page styling (#54)
    * homepage styling done
    
    * modified animation of rabbits
    
    * fixed linting errors
    
    Close #54 
    
    ---------
    
    Co-authored-by: Edwin Montalvo <edwinmontavlo123@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,62 @@
-import { faGreaterThan } from '@fortawesome/free-solid-svg-icons';
 import { FC } from 'react';
-import Button from '../components/shared/Button';
 import '../styles/Home.scss';
+import '../styles/Mario.scss';
+import '../styles/Rabbits.scss';
+import centerRabbit from '../assets/center_rabbit.png';
+import centerMeadows from '../assets/centerMeadows.png';
+import homepageTitle from '../assets/homepage_title.png';
+import homepageBacklayer from '../assets/homepageBacklayer.png';
+import leftRabbit from '../assets/leftRabbit.png';
+import marioHomepage from '../assets/mario_homepage.png';
+import rightRabbit from '../assets/rightRabbit.png';
+import sideMeadows from '../assets/sideMeadows.png';
+import startButton from '../assets/start_button.png';
 
 const Home: FC = () => {
   return (
-    <div className="home-container">
-      <span>Home Page.</span>
-      <Button icon={faGreaterThan} className="next-btn" navigation="/intro" />
+    <div
+      className="home-container"
+      style={{
+        backgroundImage: `url(${homepageBacklayer})`,
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="start-button-container">
+        <div className="start-button">
+          <a href="/intro">
+            <img src={startButton} alt="Start Button" />
+          </a>
+        </div>
+      </div>
+      <div>
+        <img className="mario-image" src={marioHomepage} alt="Mario" />
+      </div>
+      <div>
+        <img
+          className="homepage-title"
+          src={homepageTitle}
+          alt="Homepage Title"
+        />
+      </div>
+      <div>
+        <img className="center-rabbit" src={centerRabbit} alt="Center Rabbit" />
+      </div>
+      <div>
+        <img className="left-rabbit" src={leftRabbit} alt="Left Rabbit" />
+      </div>
+      <div>
+        <img className="right-rabbit" src={rightRabbit} alt="Right Rabbit" />
+      </div>
+      <div>
+        <img
+          className="center-meadows"
+          src={centerMeadows}
+          alt="Center Meadows"
+        />
+      </div>
+      <div>
+        <img className="side-meadows" src={sideMeadows} alt="Side Meadows" />
+      </div>
     </div>
   );
 };
