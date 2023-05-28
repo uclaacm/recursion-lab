782694c91b2bde94543f280356762521c86e8b49
Ashley Le <53335012+lediemhang87@users.noreply.github.com>
2023-05-27 19:42:55 -0700

    Fix linter

--- src/components/bunny-comps/BunnyUnderstandingProblem.tsx
+++ src/components/bunny-comps/BunnyUnderstandingProblem.tsx
@@ -7,7 +7,6 @@ import rabbitGroup5 from '../../assets/bunnies/rabbitgroup5.png';
 import rabbitGroup8 from '../../assets/bunnies/rabbitgroup8.png';
 
 function UnderstandingProblem(): JSX.Element {
-
   const [currentImage, setCurrentImage] = useState(0);
   const images = [rabbitGroup2, rabbitGroup3, rabbitGroup5, rabbitGroup8];
   const nextImage = () => {
@@ -17,19 +16,21 @@ function UnderstandingProblem(): JSX.Element {
   const renderedImages = images.slice(0, currentImage + 1);
 
   return (
-    <div className='understanding-problem'>
+    <div className="understanding-problem">
       Keep clicking next to see the number of rabbits increasing through time!
-      <br/><br/>
-      <button onClick={nextImage} className='next-button'>Next</button>
-      <div className='container'>
+      <br />
+      <br />
+      <button onClick={nextImage} className="next-button">
+        Next
+      </button>
+      <div className="container">
         {renderedImages.map((image, index) => (
-          <div className='box' key={index}>
-            <img className="rabit-group"  src={image} alt=""/>
+          <div className="box" key={index}>
+            <img className="rabit-group" src={image} alt="" />
             {index !== renderedImages.length - 1 && (
               <img className="arrow-rabbit" src={arrowRabbit} alt="" />
             )}
           </div>
-
         ))}
       </div>
     </div>
