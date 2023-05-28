d7b8da6236ce11d149ce90b6f921a1419139e19c
Ashley Le <53335012+lediemhang87@users.noreply.github.com>
2023-05-27 19:55:39 -0700

    Bunnies (#87)
    * Add bunnies front-end
    
    * Change white background, use Khan card
    
    * fix linter problems
    
    * Minor update for bunnies
    
    * Run yarn lint-fix
    
    * Add bunnies front-end
    
    * Change white background, use Khan card
    
    * fix linter problems
    
    * Minor update for bunnies
    
    * Run yarn lint-fix
    
    * Updated bunny base case, dining base case and formula
    
    * Update after lint fix
    
    * Fix lint error
    
    * Run linter
    
    * Lint fix
    
    * Bunnies - new feature - animation for understanding the problem
    
    * Fix linter
    
    * Fix linter

--- src/components/bunny-comps/BunnyBaseCase.tsx
+++ src/components/bunny-comps/BunnyBaseCase.tsx
@@ -1,39 +1,60 @@
 import { useState } from 'react';
+import { options_array } from '../../types';
 import './StylesBunny.scss';
-import UnderstandingQuestion from '../../assets/bunnies/UnderstandingQuestion.png';
-
+import Dropdown from '../shared/Dropdown';
 import KhanCard from '../shared/KhanCard';
 import KhanInput from '../shared/KhanInput';
 
 function BunnyBaseCase(): JSX.Element {
-  const [isCorrect] = useState([false, false]);
+  const [isCorrect, setIsCorrect] = useState([false, false]);
   const [answerKey] = useState({
     //you can have multiple "Fill in the Blank" questions
-    question1: '8',
+    question1: 'n',
+    question2: 'dropdown1',
   });
+
+  const options2: options_array[] = [
+    {
+      value: 'dropdown1',
+      label: 'n <= 1',
+    },
+    {
+      value: 'dropdown2',
+      label: 'n > 1',
+    },
+    {
+      value: 'dropdown3',
+      label: 'n < 1',
+    },
+  ];
+
   return (
     <KhanCard
       correct="This is an explanation for when you get the answer correct." //explanation for when user gets answer correct
       incorrect="This is a hint for when you get the answer incorrect." //hint when user gets answer incorrect
       correct_answer={isCorrect} //must pass in correctness state from parent
-      index={[0]} //index for what KhanCard you are referring to
+      index={[0, 1]} //index for what KhanCard you are referring to
     >
-      <div className="understanding-problem">
-        <div className="question">
-          <p>
-            What is the nth number of rabbits?
-            <KhanInput
-              correct_answer={isCorrect} //must pass in correctness state from parent
-              index={0} //index for what KhanCard you are referring to
-              answer={answerKey.question1} //the answer from answerkey
-            />
-          </p>
-        </div>
-        <div className="questionImage">
-          <img src={UnderstandingQuestion} alt="" style={{ width: '100%' }} />
-        </div>
+      <div>
+        if
+        <Dropdown
+          options={options2}
+          correct_answer={isCorrect} //must pass in correctness array from parent
+          index={0} //index for what question # you are referring to
+          answer={answerKey.question2} //the answer from answerkey
+          update_answer={setIsCorrect} //function to update the correctness array
+        />
+        <br />
+        return
+        <KhanInput
+          correct_answer={isCorrect} //must pass in correctness array from parent
+          index={1} //index for what KhanCard you are referring to
+          answer={answerKey.question1} //the answer from answerkey
+          update_answer={setIsCorrect} //function to update the correctness array
+        />
       </div>
     </KhanCard>
   );
 }
+
 export default BunnyBaseCase;
