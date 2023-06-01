89215146650c7cf5802daeb5a67525ef450b7edd
Edwin Montalvo <edwinmontavlo123@gmail.com>
2023-05-31 21:07:08 -0700

    Add Mario Generalize the Pattern KhanCard (#106)
    * Closes #71

--- src/components/mario-comps/MarioFormula.tsx
+++ src/components/mario-comps/MarioFormula.tsx
@@ -2,25 +2,31 @@ import { useState } from 'react';
 import { options_array } from '../../types';
 import Dropdown from '../shared/Dropdown';
 import KhanCard from '../shared/KhanCard';
+import KhanInput from '../shared/KhanInput';
 
 function MarioFormula(): JSX.Element {
   const [isCorrect, setIsCorrect] = useState([false]);
   const [answerKey] = useState({
-    question1: 'dropdown2',
+    question1: 'n',
+    question2: 'recurSum(n-1)',
   });
 
   const options0: options_array[] = [
     {
-      value: 'dropdown1',
-      label: 'dropdown1',
+      value: 'recurSum(n)',
+      label: 'recurSum(n)',
     },
     {
-      value: 'dropdown2',
-      label: 'dropdown2',
+      value: 'recurSum(n-2)',
+      label: 'recurSum(n-2)',
     },
     {
-      value: 'dropdown3',
-      label: 'dropdown3',
+      value: 'recurSum(n-1)',
+      label: 'recurSum(n-1)',
+    },
+    {
+      value: 'recurSum(n+1)',
+      label: 'recurSum(n+1)',
     },
   ];
 
@@ -33,19 +39,21 @@ function MarioFormula(): JSX.Element {
         index={[0]} //index for what KhanCard you are referring to
       >
         <div>
-          <span>
-            This is a sample way to use fill in the blank. The answer for this
-            input is &quot;dropdown2&quot;. Try it out! The answer for this
-            input is
-          </span>
+          recurSum(n) =
+          <KhanInput
+            correct_answer={isCorrect}
+            index={0}
+            answer={answerKey.question1}
+            update_answer={setIsCorrect}
+          />
+          +
           <Dropdown
             options={options0}
             correct_answer={isCorrect} //must pass in correctness state from parent
-            index={0} //index for what KhanCard you are referring to
-            answer={answerKey.question1} //the answer from answerkey
+            index={1} //index for what KhanCard you are referring to
+            answer={answerKey.question2} //the answer from answerkey
             update_answer={setIsCorrect}
           />
-          .
         </div>
       </KhanCard>
     </div>
