5f34bd7dd13ccbbbf4c5e1d4806b95bf5b0eb23d
Ashley Le <53335012+lediemhang87@users.noreply.github.com>
2023-06-05 01:44:46 -0700

    Finish Bunny Code

--- src/components/bunny-comps/BunnyCode.tsx
+++ src/components/bunny-comps/BunnyCode.tsx
@@ -1,16 +1,98 @@
 import { useState } from 'react';
 import { options_array } from '../../types';
-
-import Dropdown from '../shared/Dropdown';
-import KhanCard from '../shared/KhanCard';
+import BunnyCodeDropdown from '../../components/intro_comps/intro_dropdown';
+import FinishCodeCard from '../../components/shared/FinishCode';
 
 function BunnyCode(): JSX.Element {
-  const [isCorrect, setIsCorrect] = useState([false, false, false]);
+  const [isCorrect] = useState([false, false, false]);
+  const [selectedanswer, setselectedanswer] = useState({
+    question1: 'n > 1',
+    question2: '0',
+    question3: 'n * fib(n-1)',
+  });
   const [answerKey] = useState({
     question1: 'n <= 1',
     question2: 'n',
     question3: 'fib(n-1) + fib(n-2)',
   });
+
+  function recurFib(n: number): number {
+    if (n <= 1) {
+      return n;
+    }
+    return recurFib(n-2) + recurFib(n - 1);
+  }
+
+  function selectedRecurFib(
+    startValue: number,
+    ifStatement: string,
+    ifReturnVal: string,
+    functionReturnVal: string,
+  ):number{
+    if  (ifStatement == 'n > 1'){
+      return startValue
+    }
+    else if (ifStatement == 'n <= 1' && ifReturnVal=='n' && functionReturnVal == 'fib(n-1) + fib(n-2)') {
+      return nLess1NFib(startValue)
+    }
+    else if (ifStatement == 'n <= 1' && ifReturnVal=='0' && functionReturnVal == 'fib(n-1) + fib(n-2)') {
+      return nLess10Fib(startValue)
+    }
+    else if (ifStatement == 'n <= 1' && ifReturnVal=='1' && functionReturnVal == 'fib(n-1) + fib(n-2)') {
+      return nLess11Fib(startValue)
+    }
+
+    else if (ifStatement == 'n <= 1' && ifReturnVal=='n' && functionReturnVal == 'n + fib(n-1)') {
+      return nLess1NSum(startValue)
+    }
+    else if (ifStatement == 'n <= 1' && ifReturnVal=='0' && functionReturnVal == 'n + fib(n-1)') {
+      return nLess10Sum(startValue)
+    }
+    else if (ifStatement == 'n <= 1' && ifReturnVal=='1' && functionReturnVal == 'n + fib(n-1)') {
+      return nLess11Sum(startValue)
+    }
+
+    return 0
+  }
+
+  function nLess1NFib (n:number):number{
+    if (n <= 1) {
+      return n;
+    }
+    return nLess1NFib(n-2) + nLess1NFib(n - 1);
+  }
+  function nLess10Fib (n:number):number{
+    if (n <= 1) {
+      return 0;
+    }
+    return nLess10Fib(n-2) + nLess10Fib(n - 1);
+  }
+  function nLess11Fib (n:number):number{
+    if (n <= 1) {
+      return 1;
+    }
+    return nLess11Fib(n-2) + nLess11Fib(n - 1);
+  }
+
+  function nLess1NSum (n:number):number{
+    if (n <= 1) {
+      return n;
+    }
+    return n + nLess1NSum(n-1)
+  }
+  function nLess10Sum (n:number):number{
+    if (n <= 1) {
+      return 0;
+    }
+    return n + nLess10Sum(n-1)
+  }
+  function nLess11Sum (n:number):number{
+    if (n <= 1) {
+      return 1;
+    }
+    return n + nLess11Sum(n-1)
+  }
+
 
   const options1: options_array[] = [
     {
@@ -20,11 +102,7 @@ function BunnyCode(): JSX.Element {
     {
       value: 'n > 1',
       label: 'n > 1',
-    },
-    {
-      value: 'n < 1',
-      label: 'n < 1',
-    },
+    }
   ];
   const options2: options_array[] = [
     {
@@ -47,22 +125,38 @@ function BunnyCode(): JSX.Element {
       label: 'n + fib(n-1)',
     },
     {
-      value: 'fib(n-1) * fib(n-2)',
-      label: 'fib(n-1) * fib(n-2)',
-    },
-    {
       value: 'fib(n-1) + fib(n-2)',
       label: 'fib(n-1) + fib(n-2)',
     },
   ];
 
+  const handleUpdateAnswer = (index: number, chosenAnswer: string) => {
+    setselectedanswer((prevAnswerKey) => ({
+      ...prevAnswerKey,
+      [`question${index + 1}`]: chosenAnswer,
+    }));
+  };
+
   return (
     <div>
-      <KhanCard
-        correct="Put the base case and calculate fib(n) by adding fib(n-1) and fib(n-2)"
-        incorrect="Combine the 2 code from `base case` and `recursive formula` into your answer."
-        correct_answer={isCorrect}
-        index={[0, 1, 2]}
+      <FinishCodeCard
+        // correct="Put the base case and calculate fib(n) by adding fib(n-1) and fib(n-2)"
+        // incorrect="Combine the 2 code from `base case` and `recursive formula` into your answer."
+        correct_answer={[recurFib(7) == selectedRecurFib(7,selectedanswer.question1,
+          selectedanswer.question2,
+          selectedanswer.question3)]}
+        index={0}
+        given_function={() => recurFib(7)}
+          
+        chosen_function= {() =>
+          selectedRecurFib(
+            7,
+            selectedanswer.question1,
+            selectedanswer.question2,
+            selectedanswer.question3
+          )
+        }
+        answer_key={answerKey}
       >
         <div>
           def fib(n):
@@ -72,34 +166,40 @@ function BunnyCode(): JSX.Element {
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <br />
           if
-          <Dropdown
+          <BunnyCodeDropdown
             options={options1}
             correct_answer={isCorrect}
             index={0}
             answer={answerKey.question1}
-            update_answer={setIsCorrect}
+            update_answer={(chosenAnswer: string) =>
+              handleUpdateAnswer(0, chosenAnswer)
+            }
           />
           <br />
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; return
-          <Dropdown
+          <BunnyCodeDropdown
             options={options2}
             correct_answer={isCorrect}
             index={1}
             answer={answerKey.question2}
-            update_answer={setIsCorrect}
+            update_answer={(chosenAnswer: string) =>
+              handleUpdateAnswer(1, chosenAnswer)
+            }
           />
           <br />
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; return
-          <Dropdown
+          <BunnyCodeDropdown
             options={options3}
             correct_answer={isCorrect}
             index={2}
             answer={answerKey.question3}
-            update_answer={setIsCorrect}
+            update_answer={(chosenAnswer: string) =>
+              handleUpdateAnswer(2, chosenAnswer)
+            }
           />
         </div>
-      </KhanCard>
+      </FinishCodeCard>
     </div>
   );
 }
--- src/components/dining-comps/DiningCode.tsx
+++ src/components/dining-comps/DiningCode.tsx
@@ -10,7 +10,7 @@ function DiningCode(): JSX.Element {
     question1: '0',
     question2: 'Friend.left',
     question3: 'Friend.right',
-    question4: 'Friend.val',
+    question4: 'Friend.val'
   });
 
   const options1: options_array[] = [
@@ -35,7 +35,7 @@ function DiningCode(): JSX.Element {
     {
       value: 'Friend.right',
       label: 'Friend.right',
-    },
+    }
   ];
 
   const options3: options_array[] = [
@@ -46,10 +46,12 @@ function DiningCode(): JSX.Element {
     {
       value: 'Friend.right',
       label: 'Friend.right',
-    },
+    }
   ];
 
   const options4: options_array[] = [
+
+
     {
       value: 'Friend.left.val',
       label: 'Friend.left.val',
@@ -61,18 +63,19 @@ function DiningCode(): JSX.Element {
     {
       value: 'Friend.val',
       label: 'Friend.val',
-    },
+    }
   ];
+
 
   return (
     <div>
       <KhanCard
-        correct="The sum of value for friends in the left and friends in the right and the value for current friend"
-        incorrect="Think of a mathematical operation that combines the values of friends on both sides with the value of the current friend."
-        correct_answer={isCorrect}
-        index={[0, 1]}
+      correct="The sum of value for friends in the left and friends in the right and the value for current friend"
+      incorrect="Think of a mathematical operation that combines the values of friends on both sides with the value of the current friend."
+      correct_answer={isCorrect}
+      index={[0, 1]}
       >
-        <div>
+        <div >
           def post_order_sum(Friend):
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -84,54 +87,60 @@ function DiningCode(): JSX.Element {
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <br />
+          <br/>
           return
           <Dropdown
-            options={options1}
-            correct_answer={isCorrect}
-            index={0}
-            answer={answerKey.question1}
-            update_answer={setIsCorrect}
-          />
-          <br /> <br />
+              options={options1}
+              correct_answer={isCorrect}
+              index={0}
+              answer={answerKey.question1}
+              update_answer={setIsCorrect}
+            />
+
+          <br/> <br/>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; left_sum
-          = post_order_sum(
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+
+          left_sum = post_order_sum(
           <Dropdown
-            options={options2}
-            correct_answer={isCorrect}
-            index={1}
-            answer={answerKey.question2}
-            update_answer={setIsCorrect}
-          />
-          )
-          <br />
+              options={options2}
+              correct_answer={isCorrect}
+              index={1}
+              answer={answerKey.question2}
+              update_answer={setIsCorrect}
+            />)
+
+          <br/>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+
           right_sum = post_order_sum(
           <Dropdown
-            options={options3}
-            correct_answer={isCorrect}
-            index={2}
-            answer={answerKey.question2}
-            update_answer={setIsCorrect}
-          />
-          )
-          <br /> <br />
+              options={options3}
+              correct_answer={isCorrect}
+              index={2}
+              answer={answerKey.question2}
+              update_answer={setIsCorrect}
+          />)
+
+          <br/> <br/>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; return
-          (left_sum + right_sum +
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          return (left_sum + right_sum +
           <Dropdown
-            options={options4}
-            correct_answer={isCorrect}
-            index={3}
-            answer={answerKey.question4}
-            update_answer={setIsCorrect}
-          />
-          )
+              options={options4}
+              correct_answer={isCorrect}
+              index={3}
+              answer={answerKey.question4}
+              update_answer={setIsCorrect}
+          />)
+
+
         </div>
       </KhanCard>
     </div>
