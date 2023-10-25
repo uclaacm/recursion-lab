b994b313e702564be3669bd71837fac43182306e
23clarej <23clarej@students.harker.org>
2023-10-24 18:31:59 -0700

    changed KhanCard dummy text in Mario page

--- src/components/mario-comps/MarioBaseCase.tsx
+++ src/components/mario-comps/MarioBaseCase.tsx
@@ -8,7 +8,7 @@ function MarioBaseCase(): JSX.Element {
   const [isCorrect, setIsCorrect] = useState([false, false]);
   const [answerKey] = useState({
     //you can have multiple "Fill in the Blank" questions
-    question1: 'n',
+    question1: '1',
     question2: 'dropdown1',
   });
 
@@ -29,8 +29,8 @@ function MarioBaseCase(): JSX.Element {
 
   return (
     <KhanCard
-      correct="This is an explanation for when you get the answer correct." //explanation for when user gets answer correct
-      incorrect="This is a hint for when you get the answer incorrect." //hint when user gets answer incorrect
+      correct="The staircase should end at the top step with only one block. Thus, we return the value of 1 itself." //explanation for when user gets answer correct
+      incorrect="Think about the last number you add when ascending the staircase. That is the base case value. What does it return?" //hint when user gets answer incorrect
       correct_answer={isCorrect} //must pass in correctness state from parent
       index={[0, 1]} //index for what KhanCard you are referring to
     >
