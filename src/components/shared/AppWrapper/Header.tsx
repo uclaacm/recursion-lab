c1f9a7a5db4725e26df45eb03d92f8c9049dd8c1
kesdlvi <kesdlvi12@gmail.com>
2023-10-29 17:58:29 -0700

    Refactored header and sidebar and added close sidebar on outside click

--- src/components/shared/AppWrapper/Header.tsx
+++ src/components/shared/AppWrapper/Header.tsx
@@ -1,9 +1,8 @@
-import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { HeaderSections } from '../globalTypes';
-import { SidebarData } from '../SidebarData';
+import Sidebar from '../Sidebar';
 import '../../../../src/styles/AppWrapper.scss';
 
 /**
@@ -33,26 +32,7 @@ export default function Header(props: HeaderProps): JSX.Element {
         <h1>{props.section}</h1>
         <div className="box"></div>
       </div>
-      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <FontAwesomeIcon
-          icon={faX}
-          size="xl"
-          onClick={showSidebar}
-          className="close-btn"
-        />
-        <ul className="nav-menu-items" onClick={showSidebar}>
-          {SidebarData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icons}
-                  <span className="sidebar-titles">{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <Sidebar sidebar={sidebar} setSidebar={setSidebar}></Sidebar>
     </>
   );
 }
