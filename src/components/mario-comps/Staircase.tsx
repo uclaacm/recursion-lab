38574e3d9d28d91ba9de7dd841de31cc6a2fa822
Edwin Montalvo <edwinmontavlo123@gmail.com>
2023-12-30 13:22:29 -0800

    Update interactive staircase visuals

--- src/components/mario-comps/Staircase.tsx
+++ src/components/mario-comps/Staircase.tsx
@@ -51,17 +51,17 @@ function Staircase(): JSX.Element {
   return (
     <div className="staircase-body">
       <p className="mario-step1-subtitle">
-        This stage is meant for you to first see the big picture and find a way
-        to reduce it into smaller problems that reduce into base cases. This
-        interactive staircase should help you see all the steps that make up the
-        staircase, so have fun playing with it. Don&apos;t forget to see the
-        relationship between one step to the next (literally)!
+        Try to see the big picture and find a way to reduce it into smaller
+        problems that reduce into base cases. This interactive staircase should
+        help you see all the steps that make up the staircase, so have fun
+        playing with it. Don&apos;t forget to see the relationship between one
+        step to the next (literally)!
       </p>
-      <div>
-        <b className="goal">GOAL:</b>
-        <span> Find the sum of the first n natural numbers.</span>
+      <br />
+      <div className="goal-container">
+        <div className="goal">Find the sum of the first n natural numbers.</div>
       </div>
-      <p>Use the slider to set the number of steps: {numColumns}</p>
+      <p>Use the slider to set the value of n: {numColumns}</p>
       <div className="interactive-staircase">
         <div className="n-input-container">
           <input
@@ -86,10 +86,53 @@ function Staircase(): JSX.Element {
             <option value="9"></option>
           </datalist>
         </div>
+        <div className="tickmark-container">
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 1 ? <b>1</b> : 1}
+          </div>
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 2 ? <b>2</b> : 2}{' '}
+          </div>
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 3 ? <b>3</b> : 3}{' '}
+          </div>
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 4 ? <b>4</b> : 4}{' '}
+          </div>
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 5 ? <b>5</b> : 5}{' '}
+          </div>
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 6 ? <b>6</b> : 6}
+          </div>
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 7 ? <b>7</b> : 7}{' '}
+          </div>
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 8 ? <b>8</b> : 8}{' '}
+          </div>
+          <div className="tickmark-label">
+            {' '}
+            {numColumns == 9 ? <b>9</b> : 9}{' '}
+          </div>
+        </div>
+        <br />
         <Triangle numColumns={numColumns} />
+        <br />
         <span>
-          {numColumns > 1 ? `SUM = ${sumEnumerated} = ${sum}` : `SUM = ${sum}`}
+          <b>SUM </b>
+          {numColumns > 1 ? `= ${sumEnumerated} = ` : '= '}
+          <b>{`${sum}`}</b>
         </span>
+        <br />
       </div>
     </div>
   );
