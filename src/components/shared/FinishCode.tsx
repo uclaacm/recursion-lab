6e000dfc6ee8646380d26cb33a43ebf4c251bc56
rosiechen1005 <chenherosalinda@gmail.com>
2024-01-08 23:28:59 -0800

    checkmark

--- src/components/shared/FinishCode.tsx
+++ src/components/shared/FinishCode.tsx
@@ -81,7 +81,24 @@ function FinishCodeCard(props: FinishCodeCardProps): JSX.Element {
     >
       <div className="finish-title">
         {correct ? (
-          <CheckCircleIcon sx={{ color: 'green', fontSize: 'inherit' }} />
+          <svg
+            className="checkmark"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 52 52"
+          >
+            <circle
+              className="checkmark-circle"
+              cx="26"
+              cy="26"
+              r="25"
+              fill="none"
+            />
+            <path
+              className="checkmark-check"
+              fill="none"
+              d="M14.1 27.2l7.1 7.2 16.7-16.8"
+            />
+          </svg>
         ) : (
           <CheckCircleIcon sx={{ color: 'grey', fontSize: 'inherit' }} />
         )}
